422981b39d52803f79b32dd7d52499d6d23778ee
moon <bestmjh47@naver.com>
2014-02-16 20:45:16 +0900

    Edit script...

---
+++ build_kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+############################
+# Take Tachy Kernel Source #             
+#                          #
+#         bestmjh47        #
+############################
+TOOLCHAINPATH=/home/moon/toolchain/arm-eabi-4.4.3/bin
+export ARCH=arm
+#export CROSS_COMPILE=/home/moon/toolchain/arm-linux-androideabi-4.6/bin/arm-linux-androideabi-
+export CROSS_COMPILE=$TOOLCHAINPATH/arm-aebi-
+#export CROSS_COMPILE=/home/moon/toolchain/android-toolchain-eabi-4.8/bin/arm-eabi-
+make msm8660_o4_es2_defconfig
+echo #############################
+echo #       Now Starting...     #
+echo #############################
+make -j15
+echo Compiling Finished!
+cp arch/arm/boot/zImage zImage
+echo Striping Modules...
+rm -rf modules/*
+find -name '*.ko' -exec cp -av {} Modules \;
+        for i in Modules/*; do $TOOLCHAINPATH/arm-eabi-strip --strip-unneeded $i;done;\
+echo ""
+echo Done! zImage and modules are READY!!!
