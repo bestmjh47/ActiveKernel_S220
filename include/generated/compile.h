a7bae829135c4edb401e2d34e8b3b79ad7413f21
moon <bestmjh47@naver.com>
2014-02-17 17:18:46 +0900

    Revert "Bump to Linux-3.0.15"
    This reverts commit a9e7ed00287e30124f22cd86e683092326a22a10.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Mon Feb 17 16:36:21 KST 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Feb 17 16:18:54 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.15-perf-g27da0c0-dirty"
+#define UTS_RELEASE "3.0.8-perf-ga88c07e-dirty"
