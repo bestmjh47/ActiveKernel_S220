0da4b2ac25fd9cedbd124f577de0be09790e20fe
moon <bestmjh47@naver.com>
2014-02-18 13:47:05 +0900

    Linux-3.0.12

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Tue Feb 18 13:17:24 KST 2014"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Feb 18 13:34:45 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.10-Active-Kernel-S220-gbef9e54-dirty"
+#define UTS_RELEASE "3.0.12-Active-Kernel-S220-g0e81c1c-dirty"
