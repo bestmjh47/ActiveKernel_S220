6850e741f9be9ca4750364710744abfe1c2c6209
moon <bestmjh47@naver.com>
2014-02-19 15:39:23 +0900

    Add cfq,row,zen,vr

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 17 */
+/* This file is auto generated, version 260 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#17 SMP PREEMPT Wed Feb 19 15:16:46 KST 2014"
+#define UTS_VERSION "#260 SMP PREEMPT Wed Feb 19 15:37:50 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.101-Active-Kernel-S220-bestmjh47-g021fe79-dirty"
+#define UTS_RELEASE "3.0.101-Active-Kernel-S220-bestmjh47-g782e890-dirty"
