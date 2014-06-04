5d2fcf9f12c5e860e88e1e521f201f2eadaac8b0
moon <bestmjh47@naver.com>
2014-06-04 14:47:49 +0900

    Clean up...

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 261 */
+/* This file is auto generated, version 262 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#261 SMP PREEMPT Wed Feb 19 15:50:33 KST 2014"
+#define UTS_VERSION "#262 SMP PREEMPT Thu Feb 20 12:54:33 KST 2014"
 #define LINUX_COMPILE_BY "moon"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.101-Active-Kernel-S220-bestmjh47-g6850e74-dirty"
+#define UTS_RELEASE "3.0.101-Active-Kernel-S220-bestmjh47-g1fc6bf0-dirty"
